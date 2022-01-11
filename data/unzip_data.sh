ac37430b054732dbacfc490bd1ecc14a725d122f
celiadulucinsa <duluc@etud.insa-toulouse.fr>
2022-01-11 17:02:47 +0100

    create a data folder

---
+++ data/unzip_data.sh
@@ -0,0 +1,5 @@
+unzip data_station/X_station_train.csv.zip
+unzip data_station/Y_train.csv.zip
+unzip data_station/X_station_test.csv.zip
+unzip data_station/Baseline_observation_test.csv.zip
+unzip data_station/stations_coordinates.csv.zip
