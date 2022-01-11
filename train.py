38f1e5c9ae2de82c84c3e7d507369496afa4625a
celiadulucinsa <78207633+celiadulucinsa@users.noreply.github.com>
2022-01-11 16:36:45 +0100

    Update train.py

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import argparse
 import os, sys
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
 sys.path.append("utils/")
 
 
@@ -12,7 +14,9 @@ sys.path.append("utils/")
 # import features_engineering
 import training 
 import prediction
+import pre_traitement
 import preprocessing
+import model
 
 def get_args(): 
 	parser = argparse.ArgumentParser(description="Script to launch defi-ia training", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -36,16 +40,50 @@ def main():
  	else: #option2
 		df_train = pd.read_csv("df_train_nan_by_missforest.csv")  
 		df_X_test = pd.read_csv("df_X_test_nan_by_missforest.csv") 
-
-
-	X_train = df_train.drop(["Ground_truth", "Id", "number_sta", "date"], axis = 1)
-	X_test =  df_X_test.drop(["number_sta", "date"], axis = 1)
-	y_train = df_train["Ground_truth"]
+		
+	
+	# Split the training dataset
+	X_train, X_val, y_train, y_val = train_test_split(df_train.drop(["Ground_truth"], axis = 1),
+                                                    df_train["Ground_truth"],
+                                                    test_size=0.10, shuffle=True)
+	
+	X_train = X_train.drop(["Id", "number_sta", "date"], axis = 1)
+	X_val = X_val.drop(["number_sta", "date"], axis = 1)
+	X_test = df_X_test.drop(["number_sta", "date"], axis = 1)
+	
+	# Clip the outliers for the response variable
+	min_treshold, max_treshold = pre_traitement.find_outliers(y_train, 1.96)
+	y_train = y_train.clip(upper = max_treshold)
 	
 	# Normalisation 
+	var_to_fit = ['ff', 't', 'td', 'hu', 'dd', 'precip', 'lat', 'lon', 'height_sta',
+       'forecast_2D_arome_ws', 'forecast_2D_arome_p3031',
+       'forecast_2D_arome_u10', 'forecast_2D_arome_v10',
+       'forecast_2D_arome_t2m', 'forecast_2D_arome_d2m', 'forecast_2D_arome_r',
+       'forecast_2D_arome_tp', 'forecast_2D_arome_msl',
+       'forecast_2D_arpege_ws', 'forecast_2D_arpege_p3031',
+       'forecast_2D_arpege_u10', 'forecast_2D_arpege_v10',
+       'forecast_2D_arpege_t2m', 'forecast_2D_arpege_d2m',
+       'forecast_2D_arpege_r', 'forecast_2D_arpege_tp',
+       'forecast_2D_arpege_msl', 'forecast_arpege_3D_height_20',
+       'forecast_arpege_3D_height_100', 'forecast_arpege_3D_height_500',
+	'forecast_arpege_3D_height_875', 'forecast_arpege_3D_height_1375',
+	'forecast_arpege_3D_height_2000', 'forecast_arpege_3D_height_3000']
+	
+	sts = StandardScaler()
+	sts.fit(X_train[var_to_fit])
+
+	X_train[var_to_fit] = sts.transform(X_train[var_to_fit])
+	X_val[var_to_fit] = sts.transform(X_val[var_to_fit])
+	X_test[var_to_fit] =  sts.transform(X_test[var_to_fit])
 
  	# Model training
 	model_trained = training.main(X_train, y_train) 
+	
+	# Evaluation of the model on the validation dataset
+	y_pred = model_trained.predict(X_val)
+	print("R2 score on the validation dataset: ", metrics.r2_score(y_val, y_pred))
+	print("MAPE score on the validation dataset: ", model.MAPEVal(y_pred, y_val.to_numpy())) 
 	
 	# Prediction on test + save 
 	filename = output_folder + "/submission.csv"
